a8b11a32e4898d8961511a48ec8732fb98fce8e1
Boris Letocha <boris.letocha@gmail.com>
2015-10-22 01:01:52 +0200

    Easier require for bobril-build bundler.

--- index.js
+++ index.js
@@ -6,5 +6,3 @@ __export(require('./src/msgFormatter'));
 __export(require('./src/jsonp'));
 __export(require('./src/localeDataStorage'));
 __export(require('./src/translate'));
-require('numeral');
-window.moment = require('moment');
